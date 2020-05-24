725a33ab10e2d6b0706e20f84e46ad0b6247e960
Yusuke Ka <yusuke.ka@dummy.com>
2020-05-24 16:21:24 +0900

    ignoreするelementを修正

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Vue from "vue";
 import vueCustomElement from "vue-custom-element";
 import Button from "./button.vue";
 
-Vue.config.ignoredElements = ["example-component"];
+Vue.config.ignoredElements = ["vce-button"];
 Vue.use(vueCustomElement);
 Vue.customElement("vce-button", Button, {
   // shadowDOM化すると中のCSSが効かず、ここで指定したCSSが効く
